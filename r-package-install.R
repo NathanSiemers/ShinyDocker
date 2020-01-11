0e5fa5b5894e71edef02c821c1714c5295c49424
Nathan Siemers <nathan@fiveprime.org>
2020-01-11 00:53:26 +0000

    fixed r install

--- r-package-install.R
+++ r-package-install.R
@@ -11,80 +11,42 @@ qwc = function(...) { as.character( unlist( as.list( match.call() )[ -1 ] ) ) }
 install_it = function() {
 install.packages( qwc (
 
-
-<<<<<<< HEAD
-=======
-install.packages('devtools')
-install.packages('Rtsne')
-install.packages('openssl')
-install.packages('ggthemes')
-install.packages('ggplot2')
-install.packages('GGally')
-install.packages('knitr')
-install.packages('ppcor')
-install.packages("doParallel")
-install.packages('GEOquery')
-install.packages('boot')
-install.packages('grid')
-install.packages('gridExtra')
-install.packages('pheatmap')
-install.packages('plotly')
-install.packages('plyr')
-install.packages('ppcor')
-install.packages('reshape2')
-install.packages('stats')
-install.packages('Biostrings')
-install.packages('DT')
-install.packages('Hmisc')
-install.packages('RCurl')
-install.packages('amap')
-install.packages('bigmemory')
-install.packages('data.table')
-install.packages('doMC')
-install.packages('foreach')
-install.packages('knitr')
-install.packages('rmarkdown')
-install.packages('magrittr')
-install.packages('org.Hs.eg.db')
-install.packages('pheatmap')
-install.packages('plot')
-install.packages('plotly')
-install.packages('plotrix')
-install.packages('plyr')
-install.packages('ppcor')
-install.packages('psych')
-install.packages('qgraph')
-install.packages('reshape2')
-install.packages('rmarkdown')
-install.packages('stringdist')
-##install.packages('shiny', repos='https://cran.rstudio.com/')
-install.packages('shinythemes')
-install.packages('glue')
-install.packages('shinycssloaders')
->>>>>>> b0f934da879ce1f5fd4da5ac46131d33930c780b
-
-devtools,
 Biostrings,
 DT,
 GEOquery,
 GGally,
+GGally,
 Hmisc,
+RColorBrewer,
+RCurl,
 RCurl,
 RMySQL,
 Rtsne,
+Rtsne,
+Seurat,
 amap,
 bigmemory,
 boot,
 data.table,
 devtools,
+devtools,
 doMC,
 doParallel,
+editData,
+forcats,
 foreach,
+ggplot2,
 ggpubr,
+ggridges,
+ggridges,
 ggthemes,
 glue,
 grid,
 gridExtra,
+gtable,
+knitr,
+magrittr,
+openssl,
 openssl,
 org.Hs.eg.db,
 pheatmap,
@@ -93,24 +55,27 @@ plot,
 plotly,
 plotrix,
 plyr,
+pool,
 ppcor,
 psych,
 qgraph,
+qlcMatrix,
 reshape2,
+rmarkdown,
+scales,
+shinyBS,
+shinybusy,
 shinycssloaders,
+shinydashboard,
 shinyjs,
 shinythemes,
 stats,
 stringdist,
-viridis,
-Seurat,
-qlcMatrix,
-pool,
+stringr,
 tidyverse,
-shinydashboard,
-ggridges
+viridis
 
+) )
 
-) ) }
 
 install_it()
